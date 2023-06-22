95852452d057f035bc1e05886cd3f06e63ae0004
Julian Ng <julesnuggy@gmail.com>
2023-06-22 12:24:44 +0100

    Create function to send request to OpenAI

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+const makeRequest = async (url, data) => {
+  const response = await fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${_CONFIG_.API_KEY}`,
+      // Our _CONFIG data is imported in the HTML file using the <script> tag meaning
+      // it is then accessible via global scope (other JS files, browser console, etc).
+    },
+    method: 'POST',
+    body: JSON.stringify(data)
+  });
+
+  return response.json();
+}
+
 const init = () => {
   const genres = document.querySelectorAll('.genre');
   genres.forEach((button) => button.addEventListener(
